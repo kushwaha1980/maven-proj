bf56490cffb7069d11116e13f98a7a21700c5898
ee_deepakkumar@rediffmail.com <ee_deepakkumar@rediffmail.com>
2022-04-13 15:14:51 +0000

    lajfla

--- build/mvn.sh
+++ build/mvn.sh
@@ -5,4 +5,4 @@ echo "*******************************"
 
 workspace=/var/jenkins_home/workspace/maven-proj
 
-docker run --rm --user "$(id -u):$(id -g)" -v $workspace/app:/app -v /root/.m2:/root/.m2 -w /app maven:3.6-alpine "$@"
+docker run --rm -v $workspace/app:/app -v /root/.m2:/root/.m2 -w /app maven:3.6-alpine "$@"
