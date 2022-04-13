1568cf69b5f12dafc853987860d46baa068842ae
ee_deepakkumar@rediffmail.com <ee_deepakkumar@rediffmail.com>
2022-04-13 14:26:34 +0000

    aljlf

--- build/mvn.sh
+++ build/mvn.sh
@@ -3,6 +3,6 @@ echo "*******************************"
 echo "******building-docker-image****"
 echo "*******************************"
 
-workspace=/home/ec2-user/jenkins/jenkins_home/workspace/maven-proj
+workspace=/var/jenkins_home/workspace/maven-proj
 
 docker run --rm --user "$(id -u):$(id -g)" -v $workspace/app:/app -v /root/.m2:/root/.m2 -w /app maven:3.6-alpine "$@"
