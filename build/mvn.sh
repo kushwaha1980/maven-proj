888543bcdaeff0b40aa9ba8c99a816b7699e668d
EC2 Default User <ec2-user@ip-172-31-32-207.ap-south-1.compute.internal>
2022-04-13 11:32:11 +0000

    dfd

--- build/mvn.sh
+++ build/mvn.sh
@@ -3,6 +3,6 @@ echo "*******************************"
 echo "******building-docker-image****"
 echo "*******************************"
 
-workspace=/var/jenkins_home/workspace/maven-proj
+workspace=/home/ec2-user/jenkins/jenkins_home/workspace/maven-proj
 
 docker run --rm -v $workspace/app:/app -v /root/.m2:/root/.m2 -w /app maven:3.6-alpine "$@"
